d6d9d89a2025d4d23fec17e526dcfc0aa5801326
Muhammad Zaynurroyhan <royhanm35@gmail.com>
2026-08-25 15:58:32 +0700

    perf: self-host Inter and Poppins with @fontsource

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
 import "./index.css";
 import App from "./App.jsx";
 import LanguageProvider from "./i18n/LanguageProvider.jsx";
